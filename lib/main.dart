81734426cf7fbc6496cd725945e61afe5216ac64
bharathkotthakonda <bharathkotthakonda@gmail.com>
2022-03-14 22:11:55 +0530

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,10 @@
+
+
+
+
+
+
+
 import 'package:fanddapidata_using_getx/common_module/AppColors.dart';
 import 'package:fanddapidata_using_getx/common_module/AppStrings.dart';
 import 'package:flutter/material.dart';
@@ -5,9 +12,20 @@ import 'package:get/get.dart';
 
 import 'product_module/views/product_list_view.dart';
 
+
+
+
 void main() {
+    // HttpOverrides.global = new MyHttpOverrides();
   runApp( MyApp());
 }
+// class MyHttpOverrides extends HttpOverrides{
+//   @override
+//   Future<HttpClient> createHttpClient(SecurityContext context) async {
+//     return super.createHttpClient(context)
+//       ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
